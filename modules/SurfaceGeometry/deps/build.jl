d3490b1833cd0411c106ab9d3715be6ed75377b9
Janis Erdmanis <akels14@gmail.com>
2016-08-15 23:43:13 +0300

    a fix

--- modules/SurfaceGeometry/deps/build.jl
+++
@@ -1,4 +0,0 @@
-cd(Pkg.dir("SurfaceGeometry","src","libraries","cgal"))
-run(`g++ -fPIC -shared cgal_mesh.cpp -o libcgalmesh.so -lmpfr -lgmp -lCGAL -lboost_thread -frounding-math`)
-cd(Pkg.dir("SurfaceGeometry","src","libraries","eltopo-wrapper"))
-run(`make eltopowrap`)
