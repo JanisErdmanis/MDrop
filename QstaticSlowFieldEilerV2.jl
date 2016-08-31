fd7cd9a0569b71f20094c610241c27188a67c882
Janis Erdmanis <akels14@gmail.com>
2016-08-31 12:30:02 +0300

    Attacking quasistatic field increase

---
+++ QstaticSlowFieldEilerV2.jl
@@ -0,0 +1,145 @@
+using JLD
+using SurfaceGeometry
+
+include("modules/field.jl")
+include("modules/velocity.jl")
+
+#bname = "/SlowFieldEiler/"
+
+function DropEnergy(points,faces,normals,psi,H0)
+
+    vareas = zeros(Float64,size(points,2))
+    for i in 1:size(faces,2)
+        v1,v2,v3 = faces[:,i]
+        area = norm(cross(points[:,v2]-points[:,v1],points[:,v3]-points[:,v1])) /2
+        vareas[v1] += area/3
+        vareas[v2] += area/3
+        vareas[v3] += area/3
+    end
+
+    Area = sum(vareas)
+
+    # psix = PotentialSimple(points,faces,mup,H0*[1,0,0],regularize=true)
+    # psiy = PotentialSimple(points,faces,mup,H0*[0,1,0],regularize=true)
+
+    s = 0
+
+    for xkey in 1:size(points,2)
+        #s += dot(H0/2*[psix[xkey],psiy[xkey],0],normals[:,xkey]) * vareas[xkey]
+        s += psi[xkey]*dot(H0,normals[:,xkey]) * vareas[xkey]
+    end
+
+    Es = gammap * Area
+    Em = 1/8/pi * (1 - mup) * s
+
+    ### Here I could also do the normalisation of it    
+    return Es+Em
+end
+
+# if !isdir(datadir*bname)
+#     mkdir(datadir*bname)
+# end
+
+memory = []
+E = []
+
+if con==true
+    error("Not implemented")
+    
+    info("Continuing from last simulation")
+    if !isdir(outdir) || isempty(outdir)
+        error("No previous simulation found")
+    end
+    # 107;Bm=25.jld ### when viewing find a point or semicolon
+    outfiles = readdir(outdir)
+
+    import Base.isless
+    function Base.isless(x::ASCIIString,y::ASCIIString)
+        nx = parse(Int,x[1:length(x)-4])
+        ny = parse(Int,y[1:length(y)-4])
+        return nx < ny
+    end
+
+    sort!(outfiles)
+    last = outfiles[end]
+    i = parse(Int,last[1:length(last)-4]) 
+    data = load("$outdir/$last")["memory"][end]
+    ti,points,faces = data[1],data[2],data[3]
+else
+    info("Starting fresh simulation")
+    run(`rm -rf $outdir`)
+    mkdir(outdir)
+    ti = 0
+    i = 1
+
+    Bmi = 1.
+    H0i = sqrt(Bmi*gammap/(volume(points,faces)*3/4/pi)^(1/3))
+end
+
+push!(memory,(ti,points,faces))
+volume0 = volume(points,faces)
+
+vn = zeros(points)
+tp = 0
+xp = 0
+xi = 0
+oldpoints = copy(points)
+
+while true
+    normals = Array(Float64,size(points)...);
+    NormalVectors!(normals,points,faces,i->FaceVRing(i,faces))
+
+    psi,Ht,Hn = surfacefield(points,faces,normals,mup,H0i*[cos(omega*ti),sin(omega*ti),0])
+
+    Ei = DropEnergy(points,faces,normals,psi,H0i*[cos(omega*ti),sin(omega*ti),0])
+    println("E = $Ei")
+    rV = volume(points,faces)/volume0
+    println("V/V0 = $rV")
+    push!(E,Ei)
+
+    xp = xi
+    xi = maximum(abs(vn))*(ti-tp)
+    println("$xi < $(scale/10); xi-xp=$(xp-xi)")
+    if maximum(abs(vn))*(ti-tp) < scale && ti!=tp && xp-xi>0
+        storage = [tuple(memory[i]...,E[i]) for i in 1:length(memory)]
+        save("$outdir/$i.jld","memory",storage)
+        memory = []
+        E = []
+        if Bmi>Bm
+            break
+        else
+            Bmi += 1.
+        end
+        H0i = sqrt(Bmi*gammap/(volume(points,faces)*3/4/pi)^(1/3))
+        tp = ti
+        info("Proceeding with next quasistep")
+        xi = 0
+        
+        actualdt,points,faces = improvemeshcol(oldpoints,faces,points,par)
+        oldpoints = copy(points)
+    end
+
+    tensorn = mup*(mup-1)/8/pi * Hn.^2 + (mup-1)/8/pi * Ht.^2
+    vn = InterfaceSpeedZinchenko(points,faces,tensorn,etap,gammap)
+
+    #    oldpoints = copy(points)
+    for j in 1:size(points,2)
+        points[:,j] += normals[:,j]*vn[j]*h
+    end
+    ti += h
+    i += 1
+
+    ### Can be commented out with ease
+    # if !(maximum(abs(vn))*(ti-tp) < scale && ti!=tp && xp-xi>0)
+    #     actualdt,points,faces = improvemeshcol(oldpoints,faces,points,par)
+    # end
+
+    push!(memory,(ti,copy(points),faces))
+    info("Step $i has finished")
+end
+
+### Storage for last steps
+### Makes sense only if simulation exits while loop by itself
+push!(E,NaN)
+storage = [tuple(memory[i]...,E[i]) for i in 1:length(memory)]
+save("$outdir/$i.jld","memory",storage)
--- scheduler.jl
+++ scheduler.jl
@@ -1,57 +1,61 @@
-include(homedir()*"/secrets")
+### PCS are configured from the client side
+
+# ssh-keygen
+# ssh janiserdmanis@5.179.6.144; exit
+# ssh-copy-id janiserdmanis@5.179.6.144
+# And now it should be possibel to login without password
+
+# In case of slow ssh connection times I added `UseDNS no` to /etc/ssh/sshd_config
 
 type PC
     sshlogin
-    pasword
     workingdir
     maxcpuload
     actualload
-    function PC(sshlogin,pasword,workingdir,maxcpuload)
+    function PC(sshlogin,workingdir,maxcpuload)
 
         actualload = nothing
 
-        s = @spawn readall(`sshpass -p $pasword ssh $sshlogin "cat /proc/loadavg"`)
+        s = @spawn readall(`ssh $sshlogin "cat /proc/loadavg"`)
         # wait(Timer(10))
         # if isready(s)
         s = fetch(s)
-        actualload = parse(Float64,split(s," ")[3])
+        actualload = parse(Float64,split(s," ")[1])
         # else
         #     info("Connection did not work")
         # end
         #println("pc $sshlogin has a load $actualload")
-        new(sshlogin,pasword,workingdir,maxcpuload,actualload)
+        new(sshlogin,workingdir,maxcpuload,actualload)
         ### return true ### or false depending if connection established succesfully.
     end
 end
-
 
 ### have a dictionary of computers
 
 pcs = Dict()
 
 #pcs["UX305"] = PC("janiserdmanis@192.168.137.27",PASS_PC1,"~/Documents/calculation9/",4)
-#pcs["JanisPC"] = PC("janiserdmanis@192.168.137.27",PASS_PC1,"~/Documents/calculation9/",6)
-pcs["JanisPC"] = PC("janiserdmanis@5.179.6.146",PASS_PC1,"~/Dokumenti/calculation9/",6)
-pcs["CimursPC"] = PC("janiserdmanis@5.179.6.144",PASS_PC2,"~/Documents/calculation9/",4)
+#pcs["JanisPC"] = PC("janiserdmanis@192.168.137.27","~/Documents/MDrop/",6)
+pcs["JanisPC"] = PC("janiserdmanis@5.179.6.146","~/Dokumenti/MDrop/",6)
+pcs["CimursPC"] = PC("janiserdmanis@5.179.6.144","~/Documents/MDrop/",3)
 
 ### See if command is already running
 
 ### Printing out all active simulations (for names using the previous convention), on which pc running
 
 function runpc(pc,command)
-    readall(`sshpass -p $(pc.pasword) ssh $(pc.sshlogin) "$command"`)
+    readall(`ssh $(pc.sshlogin) "$command"`)
 end
 
 function getsessions(pc)
     sesnames = Any[]
-    try
-        s1 = runpc(pc,"tmux list-sessions")
-        ll = split(s1,"\n")
-        ll = ll[1:length(ll)-1]
-        for lli in ll
-            se = search(lli,": ")[1]
-            push!(sesnames,lli[1:se-1])
-        end
+
+    s1 = runpc(pc,"tmux list-sessions 2>>/dev/null || true")
+    ll = split(s1,"\n")
+    ll = ll[1:length(ll)-1]
+    for lli in ll
+        se = search(lli,": ")[1]
+        push!(sesnames,lli[1:se-1])
     end
     return sesnames
 end
@@ -78,9 +82,10 @@ end
 
 ### Executting command on remote pc in background
 
+
 function runtmux(pc,sesname,command)
 
-    run(`sshpass -p $(pc.pasword) ssh -t $(pc.sshlogin) "cd $(pc.workingdir) && tmux new-session -d -s $sesname && tmux send-keys -t $sesname:0 \"$command\" C-m "`)
+    run(`ssh -t $(pc.sshlogin) "cd $(pc.workingdir) && tmux new-session -d -s $sesname && tmux send-keys -t $sesname:0 \"$command\" C-m "`)
 end
 
 function runtmux(sesname,command)
@@ -103,25 +108,30 @@ function runtmux(sesname,command)
     end
 end
 
+function parsesesname(command)
+    command = replace(command," ","")
+    command = replace(command,".","DOT")
+end
+
 function runtmux(command)
     sessions = getallsessions()
 
-    sensnames = [sesi for (sesi,key) in sessions]
-    if command in sesnames
+    sesnames = [sesi for (sesi,key) in sessions]
+    if parsesesname(command) in sesnames
         error("Already running")
     else
-        runtmux(command,command)
+        runtmux(parsesesname(command),command)
     end
 end
 
 ### Printing out output of simulation -> connect to tmux
     
 function pushpc(pc)
-    run(`sshpass -p $(pc.pasword) rsync -a --exclude="*/.*" --info=progress2  $BaseDir/ $(pc.sshlogin):$(pc.workingdir)`)
+    run(`rsync -a --exclude="*/.*" --exclude=".git/*" --exclude="*/METADATA/*" --exclude="*/.git/*" --info=progress2 $BaseDir/ $(pc.sshlogin):$(pc.workingdir)`)
 end
 
 function pullpc(pc)
-    run(`sshpass -p $(pc.pasword) rsync -a --no-i-r --info=progress2 $(pc.sshlogin):~/SimulationData/ $(homedir())/SimulationData`)
+    run(`rsync -a --no-i-r --info=progress2 $(pc.sshlogin):~/SimulationData/ $(homedir())/SimulationData`)
 end
 
 if length(ARGS)==0
@@ -164,7 +174,7 @@ else
         end
         ses,key = sessions[choice]
         pc = pcs[key]
-        run(`sshpass -p $(pc.pasword) ssh -t $(pc.sshlogin) "tmux attach-session -t $ses"`)    
+        run(`ssh -t $(pc.sshlogin) "tmux attach-session -t $ses"`)    
 
     elseif ARGS[1]=="push" ### Additional argument for pushing last steps to all pcs
         for pc in values(pcs)
@@ -174,6 +184,9 @@ else
         for pc in values(pcs)
             pullpc(pc)
         end
+    else
+        error("Command $(ARGS[1]) not found.")
     end
+    
 end
    
